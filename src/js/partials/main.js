0e4a9241724af52f130755204b4c503417d27754
hudson8811 <hudson88@yandex.ru>
2021-04-02 16:52:03 +0300

    changed

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-});
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+
+    $('.faq__item-heading').click(function(){
+        const $this = $(this)
+        if(!$this.hasClass('is--active')){
+            $('.faq__item-heading').removeClass('is--active')
+            $('.faq__item-desc').slideUp(200)
+            $this.addClass('is--active')
+            $this.siblings('.faq__item-desc').slideDown(200)
+        } else{
+            $this.removeClass('is--active')
+            $this.siblings('.faq__item-desc').slideUp(200)
+        }
+    })
+})
