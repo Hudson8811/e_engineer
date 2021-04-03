124a4ebd832d7fb92a62a94171aac343c218d3ab
hudson8811 <hudson88@yandex.ru>
2021-04-03 14:05:13 +0300

    changed

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -35,4 +35,42 @@ $(document).ready(function(){
             $this.siblings('.faq__item-desc').slideUp(200)
         }
     })
+
+    $('.services-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        appendDots: $('.services-slider__dots'),
+        prevArrow: $('.services-slider__prev'),
+        nextArrow: $('.services-slider__next'),
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+
+    $('.market-icon').click(function(e){
+        e.preventDefault()
+        if(!$(this).hasClass('is--active')){
+            $(this).addClass('is--active')
+            $('.basket').addClass('is--active')
+            $('.overlay').addClass('is--active')
+            $('.footer').addClass('is--active')
+            $('body').addClass('is--active')
+        } else{
+            $(this).removeClass('is--active')
+            $('.basket').removeClass('is--active')
+            $('.overlay').removeClass('is--active')
+            $('.footer').removeClass('is--active')
+            $('body').removeClass('is--active')
+        }
+    })
 })
